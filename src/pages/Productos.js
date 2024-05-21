2463bc6295ce1796ac94664fab11aef23472b3a6
DevepMau <oshiettecs@gmail.com>
2024-05-21 01:14:51 -0300

    US/12-se implemento la visualizacion de los productos desde productos.js

--- src/pages/Productos.js
+++ src/pages/Productos.js
@@ -1,11 +1,34 @@
 import { Box } from '@mui/material'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function Productos() {
+
+    const [productos, setProductos] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/productos')
+          .then(response => {
+            setProductos(response.data);
+          })
+          .catch(error => {
+            console.error('Error fetching products:', error);
+          });
+      }, []);
+
   return (
     <Box>
         <h2>Productos</h2>
-        <p>Lista de productos</p>
+        <ul>
+            {productos.map(producto => (
+                <li key={producto.id}>
+                    <h2>{producto.nombre}</h2>
+                    <p>{producto.descripcion}</p>
+                    <p>Precio: ${producto.precio}</p>
+                    <img src={producto.iimage} alt={producto.nombre} />
+                </li>
+            ))}
+        </ul>
     </Box>
   )
 }
