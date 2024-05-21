bae7d9de6fa698444e1539e15454d9336d215d29
DevepMau <oshiettecs@gmail.com>
2024-05-21 00:00:34 -0300

    us/09-Sistema de enrutamiento basico

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Productos from './pages/Productos';
+import Carrito from './pages/Carrito';
+import { Box } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box classname='app'>
+      <Router>
+        <Header />
+        <main>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route path='/productos' element={<Productos />}  />
+            <Route path='carrito' element={<Carrito />} />
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
+    </Box>
   );
 }
 
